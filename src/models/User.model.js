7346908f2080a148a7cdfd3c348bc18f16ffa722
unimatrix2 <dnny.nis@gmail.com>
2020-12-01 19:42:56 -0300

    changed cpf max length

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     firstName: { type: String, required: true, min: 3, max: 50 },
     lastName: { type: String, required: true, min: 3, max: 100 },
 	email: { type: String, required: true },
-	cpf: { type: String, required: true, min: 11, max: 11 },
+	cpf: { type: String, required: true, min: 11, max: 14 },
 	password: { type: String, required: true, min: 8, max: 200 },
 	role: { type: String, enum: ['usuário', 'moderador', 'admin'], required: true, default: 'usuário' },
 	imageURL: { type: String, default: 'https://www.ecp.org.br/wp-content/uploads/2017/12/default-avatar.png' },
