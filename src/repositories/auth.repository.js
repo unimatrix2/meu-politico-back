7014324034eb5e84083b39e13e9f903bbf9f5a76
unimatrix2 <dnny.nis@gmail.com>
2020-12-02 20:07:44 -0300

    removed commented class

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -9,23 +9,3 @@ export const saveUser = async body => {
     const newUser = new User(body);
     await newUser.save();
 }
-
-/* class AuthRepository {
-  constructor() {
-    this.User = UserEntity.User;
-  }
-
-  async findUser(cpf) {
-    const user = this.User.findOne({ cpf });
-
-    return user;
-  }
-
-  async saveUser(body) {
-    const newUser = new this.User(body);
-
-    await newUser.save();
-  }
-}
-
-export default new AuthRepository(); */
