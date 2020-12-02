5a34ba7e3830fce3cb61979bbd05e473445cb7b4
unimatrix2 <dnny.nis@gmail.com>
2020-12-01 22:11:30 -0300

    email and cpf are now unique

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -6,8 +6,8 @@ const userSchema = new Schema(
 	{
 		firstName: { type: String, required: true, min: 3, max: 50 },
 		lastName: { type: String, required: true, min: 3, max: 100 },
-		email: { type: String, required: true },
-		cpf: { type: String, required: true, min: 11, max: 14 },
+		email: { type: String, required: true, unique: true },
+		cpf: { type: String, required: true, min: 11, max: 14, unique: true },
 		password: { type: String, required: true, min: 8, max: 200 },
 		role: {
 			type: String,
