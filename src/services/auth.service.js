89c022191c4c9ad8b7d104db79e041b788850be6
unimatrix2 <dnny.nis@gmail.com>
2020-12-03 03:59:14 -0300

    implemented a fine error handling XD

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,21 +5,23 @@ import { encrypt, verify } from '../utils/passwordManager';
 import AppError from '../errors/AppError';
 
 
-const verifyExistingUser = async cpf => {
-    const user = await authRepo.findUser(cpf)
-
+const verifyExistingUser = async (cpf, email) => {
+    const user = await authRepo.findUser(cpf);
     if (user) {
-        throw new AppError({ message: 'Usuário já existe', type: 'Registro-Usuario-Existe', status: 400 });
-    } else return false;
+        if (user.cpf === cpf && user.email === email) { throw new AppError({ message: 'Usuário já existe', type: 'Registro-Usuario-Existe', status: 400 }) }
+        else if (user.cpf === cpf) { throw new AppError({ message: 'CPF já cadastrado', type: 'Registro-CPF-Existe', status: 400 }) }
+
+    }
 
 }
 
 export const register = async body => {
     try {
-        const userExists = await verifyExistingUser(body.cpf);
+        const userExists = await verifyExistingUser(body.cpf, body.email);
         if (!userExists) {
             const newUser = { ...body, password: encrypt(body.password) };
-            await authRepo.saveUser(newUser);
+            const othersEmailError = await authRepo.saveUser(newUser);
+            if (othersEmailError) { throw new AppError({ message: 'E-Mail já cadastrado', type: 'Registro-Email-Existe', status: 400 }) }
         }
     } catch (error) { throw new AppError(error) }
 }
