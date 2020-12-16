2e92ecda8011d424c66f131e2a2c0546481d6191
unimatrix2 <dnny.nis@gmail.com>
2020-12-16 18:57:01 -0300

    fixed object being passed to service

--- src/routes/noticiaRoutes/private/routes.js
+++ src/routes/noticiaRoutes/private/routes.js
@@ -9,20 +9,20 @@ const router = Router();
 router.use(routeProtection);
 
 router.post('/criar', async (req, res, next) => {
-  try {
-    const { id } = req.user;
-    const newNoticia = req.body;
+    try {
+        const { id } = req.user;
+        const newNoticia = req.body;
 
-    await noticiasService.create(newNoticia, id);
+        await noticiasService.create(newNoticia, id);
 
-    return res.status(201).json();
-  } catch (error) { return next(new AppError(error)) };
+        return res.status(201).json();
+    } catch (error) { return next(new AppError(error)) };
 });
 
 router.put('/editar/:id', async (req, res, next) => {
     try {
         const { id } = req.params;
-
+        const updateObject = req.body;
         const updatedNoticia = await noticiasService.updateOne(updateObject, id);
 
         return res.status(200).json(updatedNoticia);
