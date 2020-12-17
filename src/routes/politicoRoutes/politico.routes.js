a2c39b3bfb9d68755fd246ac99719931b7cef6c4
Cynthia Takiishi <ycynthiaakemit@gmail.com>
2020-12-17 02:27:54 -0300

    Back changes

--- src/routes/politicoRoutes/politico.routes.js
+++ src/routes/politicoRoutes/politico.routes.js
@@ -6,16 +6,17 @@ import * as politicoService from '../../services/politico.service';
 
 const router = Router();
 
-router.get('/lista', async (req, res, nxt) => {
-    try {
-        const politicos = await politicoService.getAll();
-        res.status(200).json(politicos);
-    } catch (error) { return nxt(new AppError(error)) };
+router.get('/lista/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const noticia = await politicoService.getOne(id);
+
+    return res.status(200).json(noticia);
+  } catch (error) {
+    return next(new AppError(error));
+  }
 });
-//query: listar através de um search os políticos
-// query: string
-// query: começo
-// query: fim
 
   router.use('/privado', politicoPrivate);
 
